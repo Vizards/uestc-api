80133ad997891204c309b621135958219da42b51
Vizards <gvizards@gmail.com>
2018-04-02 18:13:12 +0800

    fix: extra APIs link address

--- app/controller/extra.js
+++ app/controller/extra.js
@@ -12,31 +12,31 @@ class ExtraController extends Controller {
   async info() {
     const { ctx } = this;
     ctx.status = 302;
-    ctx.redirect('http://wx.jwc.uestc.edu.cn/wx/toIndex.action');
+    ctx.redirect('https://jwc.uestc.ga/wx/toIndex.action');
   }
 
   async stu() {
     const { ctx } = this;
     ctx.status = 302;
-    ctx.redirect('http://wx.jwc.uestc.edu.cn/wx/SchAbout!findNewsInfo.action?partId=37,62,4028811d56bccc720156bcf9a16f0004,4028811d5688c21501568db010930007');
+    ctx.redirect('https://jwc.uestc.ga/wx/SchAbout!findNewsInfo.action?partId=37,62,4028811d56bccc720156bcf9a16f0004,4028811d5688c21501568db010930007');
   }
 
   async edu() {
     const { ctx } = this;
     ctx.status = 302;
-    ctx.redirect('http://wx.jwc.uestc.edu.cn/wx/SchAbout!findNewsInfo.action?partId=4028811d54f83c720154ff69e1890002,39');
+    ctx.redirect('https://jwc.uestc.ga/wx/SchAbout!findNewsInfo.action?partId=4028811d54f83c720154ff69e1890002,39');
   }
 
   async communication() {
     const { ctx } = this;
     ctx.status = 302;
-    ctx.redirect('http://wx.jwc.uestc.edu.cn/wx/SchAbout!findNewsInfo.action?partId=38,4028811d5688c21501568daf83b20006');
+    ctx.redirect('https://jwc.uestc.ga/wx/SchAbout!findNewsInfo.action?partId=38,4028811d5688c21501568daf83b20006');
   }
 
   async news() {
     const { ctx } = this;
     ctx.status = 302;
-    ctx.redirect('http://wx.jwc.uestc.edu.cn/wx/SchAbout!findNewsInfo.action?partId=40');
+    ctx.redirect('https://jwc.uestc.ga/wx/SchAbout!findNewsInfo.action?partId=40');
   }
 
   async room() {
